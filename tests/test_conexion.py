056047057d05b9427a5f803ea1e56e5ddc8c6740
Julián Esteban Gáez Vargas <vargasjulian.2207@gmail.com>
2026-02-24 02:46:36 -0300

    CI/CD creacion

---
+++ tests/test_conexion.py
@@ -0,0 +1,8 @@
+from sqlalchemy import text
+from utils.conexion_postgre import get_engine
+
+def test_db_connection():
+    engine = get_engine()
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT 1"))
+        assert result.scalar() == 1
